721e417890c0673b5e4dd9044b78c806777ce4a6
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-09 23:34:11 +0200

    Tehtävä 3.11. Lisää frontend

---
+++ build/precache-manifest.badc7e6344725eee14d7474cb884091c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "448eb35cb1ac306ba225",
+    "url": "/static/css/main.780575c2.chunk.css"
+  },
+  {
+    "revision": "448eb35cb1ac306ba225",
+    "url": "/static/js/main.448eb35c.chunk.js"
+  },
+  {
+    "revision": "2abf9714780b74a39252",
+    "url": "/static/js/1.2abf9714.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7efc2ecd32531e6fbbb5f0a1bd2392e7",
+    "url": "/index.html"
+  }
+];
