7bf14f4230ece49cd6dfa19e29bf34e9d8e87ca7
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-10 17:17:21 +0200

    Tehtävä 3.19

--- index.js
+++ index.js
@@ -134,7 +134,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 mongoose.set('useFindAndModify', false)
 
@@ -9,9 +10,11 @@ mongoose.connect(url, {useNewUrlParser: true})
 .catch(error => console.log('Failed to connect to MongoDB'))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: { type: String, required: true, unique: true },
+  number: { type: String, required: true }
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
