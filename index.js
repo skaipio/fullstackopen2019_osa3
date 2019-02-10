3dc251adc48f533bdcd8c18f678e5c39212a6615
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-10 16:29:58 +0200

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -103,10 +103,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    console.log('deleted', result)
+    response.status(204).end();
+  })
 });
 
 app.use(express.static('build'))
